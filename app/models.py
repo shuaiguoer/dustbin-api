ef659a463004ab087b3811d3a5c425a172583282
shuaiguoer <ls12345666@qq.com>
2022-07-10 14:04:12 +0800

    更新数据模型

--- app/models.py
+++ app/models.py
@@ -18,12 +18,63 @@ class EntityBase(object):
         return fields
 
 
+class Menu(db.Model, EntityBase):
+    __tablename__ = 'menu'
+
+    id = db.Column(db.Integer, primary_key=True, info='菜单主键')
+    name = db.Column(db.String(20), nullable=False, info='菜单名称')
+    path = db.Column(db.String(50), nullable=False, info='菜单路径')
+    component = db.Column(db.String(50), nullable=False, info='菜单组件名称')
+    redirect = db.Column(db.String(255), info='菜单跳转链接')
+    title = db.Column(db.String(20), info='菜单标题')
+    icon = db.Column(db.String(50), info='菜单图标名称')
+    hidden = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='菜单: 显示(0)/隐藏(1)')
+    sort = db.Column(db.Integer, nullable=False, info='排序(越小越靠前)')
+    type = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='类型: 菜单(1) 权限(2)')
+    pid = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='父菜单ID')
+
+
+class Role(db.Model, EntityBase):
+    __tablename__ = 'role'
+
+    id = db.Column(db.Integer, primary_key=True, info='角色主键')
+    name = db.Column(db.String(20), nullable=False, info='角色名称')
+    description = db.Column(db.String(255), info='角色描述')
+
+
+class RoleMenu(db.Model, EntityBase):
+    __tablename__ = 'role_menu'
+
+    id = db.Column(db.Integer, primary_key=True, info='角色菜单表主键')
+    role_id = db.Column(db.ForeignKey('role.id'), nullable=False, index=True, info='角色ID')
+    menu_id = db.Column(db.ForeignKey('menu.id'), nullable=False, index=True, info='菜单ID')
+
+    menu = db.relationship('Menu', primaryjoin='RoleMenu.menu_id == Menu.id', backref='role_menus')
+    role = db.relationship('Role', primaryjoin='RoleMenu.role_id == Role.id', backref='role_menus')
+
+
 class User(db.Model, EntityBase):
-    __tablename__ = 'user'  # 数据库表名
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), index=True, nullable=False)
-    age = db.Column(db.Integer)
-    department = db.Column(db.String(20))
+    __tablename__ = 'user'
+
+    userId = db.Column(db.Integer, primary_key=True, info='用户主键')
+    username = db.Column(db.String(50), nullable=False, info='用户名称')
+    password = db.Column(db.String(50), nullable=False, info='用户密码')
+    email = db.Column(db.String(50), info='用户邮箱')
+    avatar = db.Column(db.String(255), info='用户头像地址')
+    gender = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='用户性别')
+    introduction = db.Column(db.String(255), info='用户介绍')
+    registration_time = db.Column(db.String(50), nullable=False, info='用户注册时间')
+
+
+class UserRole(db.Model, EntityBase):
+    __tablename__ = 'user_role'
+
+    id = db.Column(db.Integer, primary_key=True, info='用户角色映射表主键')
+    user_id = db.Column(db.ForeignKey('user.userId'), nullable=False, index=True, info='用户ID')
+    role_id = db.Column(db.ForeignKey('role.id'), nullable=False, index=True, info='角色ID')
+
+    role = db.relationship('Role', primaryjoin='UserRole.role_id == Role.id', backref='user_roles')
+    user = db.relationship('User', primaryjoin='UserRole.user_id == User.userId', backref='user_roles')
 
 
 if __name__ == '__main__':
