6e0a4df45ab19fd677f47d3fdca02f5f8e1988c7
shuaiguoer <ls12345666@qq.com>
2022-07-17 19:18:56 +0800

    新增角色字典接口

--- app/views/dict.py
+++ app/views/dict.py
@@ -16,10 +16,21 @@ from app.utils.ResponseWrap import successResponseWrap
 dictionary = Blueprint('dictionary', __name__)
 
 
+# 性别
 @dictionary.get("/dict/gender")
-def getGenderList():
+def Gender():
     data = [
-        {"label": "男", "value": 1},
         {"label": "女", "value": 0},
+        {"label": "男", "value": 1},
+    ]
+    return successResponseWrap(data=data)
+
+
+# 角色
+@dictionary.get("/dict/role")
+def Role():
+    data = [
+        {"label": "管理员", "value": 1},
+        {"label": "普通用户", "value": 2}
     ]
     return successResponseWrap(data=data)
