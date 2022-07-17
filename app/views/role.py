d109735c36c0c6968d45c62edf31d58342d75026
shuaiguoer <ls12345666@qq.com>
2022-07-17 19:17:08 +0800

    新增获取角色列表接口

--- app/views/role.py
+++ app/views/role.py
@@ -10,20 +10,38 @@ from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app import db
-from app.models import Role
+from app.models import Role, Menu, RoleMenu
 from app.utils.ResponseWrap import successResponseWrap
+from app.modules.VerifyAuth import role_required
+from app.utils.GenerateMenus import generateMenuTree, filterRoleTree
 
 role = Blueprint('role', __name__)
 
 
+# 获取角色列表
 @role.get("/role/list")
 @jwt_required()
 def getRoleList():
-    roles = Role.query.all()
-    roleList = []
-    for r in roles:
-        roleList.append({
-            "label": r.nickname,
-            "value": r.id
-        })
-    return successResponseWrap(data=roleList)
+    page = request.args.get("page", type=int)
+    pageSize = request.args.get("pageSize", type=int)
+
+    # 查询所有角色
+    roles = Role.query.slice(pageSize * (page - 1), pageSize * page).all()
+
+    roleList = [r.to_json() for r in roles]
+
+    # 查询角色总数量
+    itemCount = db.session.query(db.func.count(Role.id)).scalar()
+
+    # 获取总页数
+    pageCount = int((itemCount + pageSize - 1) / pageSize)
+
+    roleData = {
+        "list": roleList,
+        "page": page,
+        "pageSize": pageSize,
+        "pageCount": pageCount,
+        "itemCount": itemCount,
+    }
+
+    return successResponseWrap(data=roleData)
